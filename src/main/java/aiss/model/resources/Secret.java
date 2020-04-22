6040dc3bff5f6e0bfa9f37a8ece31b676ad47f16
Acunnia <carlosacunagg@gmail.com>
2020-04-22 17:02:25 +0200

    Testing 2

--- src/main/java/aiss/model/resources/Secret.java
+++ src/main/java/aiss/model/resources/Secret.java
@@ -1,6 +1,6 @@
 package aiss.model.resources;
 
 public class Secret {
-	public String SPOTIFYKEY = "no deberia cambiar";
+	public String SPOTIFYKEY = "ahora si no deberia cambiar";
 	public String SPOTIFY_SECRET = "spotify secret";
 }
