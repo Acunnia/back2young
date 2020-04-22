4640ce0ae2689dc25f0a146bdd35b474121f64bd
Acunnia <carlosacunagg@gmail.com>
2020-04-22 17:04:15 +0200

    Gitingore update

--- src/main/java/aiss/model/resources/Secret.java
+++ src/main/java/aiss/model/resources/Secret.java
@@ -1,6 +1,6 @@
 package aiss.model.resources;
 
 public class Secret {
-	public String SPOTIFYKEY = "ahora si no deberia cambiar";
+	public String SPOTIFYKEY = " deberia cambiar";
 	public String SPOTIFY_SECRET = "spotify secret";
 }
