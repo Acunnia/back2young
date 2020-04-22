54d85523e0be6ef84557294562fb0ac6be89dfec
Acunnia <carlosacunagg@gmail.com>
2020-04-22 17:57:01 +0200

    Base of apotify api

---
+++ src/main/java/aiss/controller/SpotifyController.java
@@ -0,0 +1,63 @@
+package aiss.controller;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import aiss.model.resources.SpotifyResource;
+import aiss.model.spotify.SearchTracks;
+
+public class SpotifyController extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	private static final Logger log = Logger.getLogger(SpotifyController.class.getName());
+	 
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SpotifyController() {
+        super();
+    }
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		String query = request.getParameter("searchQuery");
+		String accessToken=(String) request.getSession().getAttribute("Spotify-token");	
+		RequestDispatcher rd = null;
+		
+		if(accessToken!=null && !"".equals(accessToken)){
+
+		// Search for tracks in Spotify
+		log.log(Level.FINE, "Searching for Spotify results of " + query);
+		SpotifyResource spotify = new SpotifyResource(accessToken);
+		SearchTracks spotifyResults = spotify.getTrackSearch(query);
+		
+		if (spotifyResults!=null){
+			rd = request.getRequestDispatcher("/success2.jsp");
+			request.setAttribute("tracks", spotifyResults.getTracks().getItems());
+		} else {
+			log.log(Level.SEVERE, "Spotify object: " + spotifyResults);
+			rd = request.getRequestDispatcher("/error.jsp");
+		}
+		
+		}else{
+			log.info("Intentando acceder a Spotify sin un token de acceso, redirigiendo al OAuth servlet");
+			rd = request.getRequestDispatcher("/AuthController/Spotify");	
+		}
+		rd.forward(request, response);
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+}
--- src/main/java/aiss/model/resources/SpotifyResource.java
+++ src/main/java/aiss/model/resources/SpotifyResource.java
@@ -1,7 +1,59 @@
 package aiss.model.resources;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.restlet.data.ChallengeResponse;
+import org.restlet.data.ChallengeScheme;
+import org.restlet.data.Header;
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
+import org.restlet.util.Series;
+
+import com.google.appengine.repackaged.com.google.gson.Gson;
+
+import aiss.model.spotify.SearchTracks;
+
 public class SpotifyResource {
-	public void test() {
-		return;
+
+	private String userPlaylistsUri = "https://api.spotify.com/v1/me/playlists";
+	private String trackSearchUri = "https://api.spotify.com/v1/search?type=track&q=";
+	private String access_Token = null;
+	private static final Logger log=Logger.getLogger(SpotifyResource.class.getName());
+	
+	
+	public SpotifyResource(String access_Token) {
+		this.access_Token = access_Token;
+	}
+	
+	public SearchTracks getTrackSearch(String query) throws UnsupportedEncodingException{
+		String json = null;
+		ClientResource cr = null;
+		try{
+			cr = new ClientResource(trackSearchUri + query + "&access_token=" + access_Token);
+			json = cr.get(String.class);
+			log.log(Level.FINE,"Búsqueda realizada correctamente."+json);
+			Map<String, Object> reqAttribs = cr.getRequestAttributes(); 
+			Series<Header> headers = (Series<Header>)reqAttribs.get("org.restlet.http.headers"); 
+			if (headers == null) { 
+				headers = new Series<Header>(Header.class); 
+				reqAttribs.put("org.restlet.http.headers", headers); 
+			} 
+				headers.add(new Header("Authorization:", "Bearer "+access_Token));
+				ChallengeResponse chr = new ChallengeResponse(
+							ChallengeScheme.HTTP_OAUTH_BEARER);
+				chr.setRawValue(access_Token);
+				cr.setChallengeResponse(chr);
+				
+			}catch (ResourceException re){
+				System.err.println("Error cuando accedia a Spotify: " + cr.getResponse().getStatus());
+		}
+		 Gson gsonObj = new Gson();
+		 SearchTracks sol = gsonObj.fromJson(json, SearchTracks.class);
+         log.log(Level.FINE, "Búsqueda en Spotify realizada correctamente.");
+        
+         return sol;
 	}
 }
