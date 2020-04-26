8b4835dd7fab4af5935a7e67cf5cdbcd6635baac
Galeon2098 <fraprapra1@alum.us.es>
2020-04-26 18:36:52 +0200

    Api de youtube y clases del json

--- src/main/java/aiss/model/resources/Secret.java
+++ src/main/java/aiss/model/resources/Secret.java
@@ -1,6 +1,6 @@
 package aiss.model.resources;
 
 public class Secret {
-	public String SPOTIFYKEY = "apikey deberia cambiar";
-	public String SPOTIFY_SECRET = "spotify secret";
+	public String SPOTIFYKEY = "db741bc952784b499b08bd0a236d4829";
+	public String SPOTIFY_SECRET = "2071968f343a46a9a790acd47fc5e1fc";
 }
