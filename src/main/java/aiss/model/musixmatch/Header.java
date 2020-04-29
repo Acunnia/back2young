ca102017414d45e79fafedd5f2f93f45d66a743b
Galeon2098 <fraprapra1@alum.us.es>
2020-04-29 18:31:21 +0200

    musixmatch resources creado

---
+++ src/main/java/aiss/model/musixmatch/Header.java
@@ -0,0 +1,57 @@
+
+package aiss.model.musixmatch;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "status_code",
+    "execute_time"
+})
+public class Header {
+
+    @JsonProperty("status_code")
+    private Integer statusCode;
+    @JsonProperty("execute_time")
+    private Double executeTime;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("status_code")
+    public Integer getStatusCode() {
+        return statusCode;
+    }
+
+    @JsonProperty("status_code")
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    @JsonProperty("execute_time")
+    public Double getExecuteTime() {
+        return executeTime;
+    }
+
+    @JsonProperty("execute_time")
+    public void setExecuteTime(Double executeTime) {
+        this.executeTime = executeTime;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
