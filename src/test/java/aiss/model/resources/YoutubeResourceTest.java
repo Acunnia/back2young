69b02ae5db49df73688a1230c2dac946f729695d
Juanmagc99 <juagarcri@alum.us.es>
2020-04-29 20:50:17 +0200

    Test Para la API de Youtube

---
+++ src/test/java/aiss/model/resources/YoutubeResourceTest.java
@@ -0,0 +1,24 @@
+package aiss.model.resources;
+
+import org.junit.Test;
+
+import aiss.model.youtube.VideoMusicSearch;
+import static org.junit.Assert.*;
+import java.io.UnsupportedEncodingException;
+
+
+
+public class YoutubeResourceTest {
+
+	@Test
+	public void getVideoTest() throws UnsupportedEncodingException {
+		String titulo = "BlackPink - Kill This Love";
+		YoutubeResource youtube = new YoutubeResource();
+		VideoMusicSearch youtubeResults = youtube.getVideo(titulo);
+		
+		assertNotNull("The search returned null", youtubeResults);
+		assertNotNull("The search returned null", youtubeResults.getItems().get(0).getSnippet().getTitle());
+		assertFalse("The number of videos is zero", youtubeResults.getItems().size()==0);
+	}
+	
+}
